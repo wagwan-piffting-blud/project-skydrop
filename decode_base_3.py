4eb959fbc2addb217e8068a2492779c7c9017546
wagwan piffting <68403205+wagwan-piffting-blud@users.noreply.github.com>
2024-10-19 19:44:31 -0500

    Update requirements, decode_base_3

--- decode_base_3.py
+++ decode_base_3.py
@@ -35,13 +35,26 @@ def decode_base3_to_ascii_long(base3_string):
             decoded_strings.append(decoded_string)
         return decoded_strings
 
-SEQU = input("Paste your base-3 here: ")
-triplet = SEQU
-print("STRING: " + str(triplet))
-final = decode_base3_to_ascii_long(triplet)
+"""
+Some example strings to try:
 
-if isinstance(final, list) and len(final) > 1:
-    pass
-else:
-    print("\nFINAL: " + final[0])
-print("\n" + "-" * 80 + "\n")
+0100202220121212 - Coin
+
+2011102202012022211110222010221010002222121111002200222220222011122001010001011100022221010211220202 - Trophy 2nd Big Ring, Row/Message 1
+
+2011201120212210000101111111010110010101002020202100110122221112211120210001210011111212021212000200 - Trophy 2nd Big Ring, Row/Message 2
+
+1201211022111001002102010212102222010102001000220022200100210021220102210220012002021121002112020112 - Trophy 2nd Big Ring, Row/Message 3
+
+"""
+
+while True:
+    SEQU = input("Paste your base-3 here: ")
+    print("STRING: " + str(SEQU))
+    final = decode_base3_to_ascii_long(SEQU)
+
+    if isinstance(final, list) and len(final) > 1:
+        pass
+    else:
+        print("\nFINAL: " + final[0])
+    print("\n" + "-" * 80 + "\n")
